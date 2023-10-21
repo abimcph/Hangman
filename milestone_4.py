3fc781cd8aab06103580ad8982500cc52bf50140
abimcph <abimcph@yahoo.com>
2023-10-22 00:02:06 +0100

    Defined is_game_over in hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -1,45 +1,55 @@
-# Create a class called hangman
 import random
 
-class hangman:
+class Hangman:
     def __init__(self, word_list, num_lives=5):
-        #initialise attributes
         self.word_list = word_list
-        self.word = random.choice(word_list)
-        self.word_guessed = ['_' for _ in self.word]
-        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
         self.list_of_guesses = []
-    
+
     def check_guess(self, guess):
-        # convert the guessed letter to lower case
-        guess = guess. lower()
-
-        # create an if statement that checks if the guess is in the word
-        if guess in self.word:
-            print(f"Good guess! '{guess}' is in the word.")
-
-            for i, letter in enumerate(self.word):
-                if letter == guess:
+        guess = guess.lower()
+        if guess in self.list_of_guesses:
+            print(f"You already guessed '{guess}'. Try again.")
+            return False
+        elif guess in self.word:
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
                     self.word_guessed[i] = guess
-        
-            self.num_letters -= 1
-
+            self.list_of_guesses.append(guess)
+            return True
         else:
-            pass
-    
-    def ask_for_input(self):
-        while True:
-            guess = input("Guess a letter:")
+            self.num_lives -= 1
+            self.list_of_guesses.append(guess)
+            return False
 
-            if not (guess.isalpha() and len(guess) == 1):
-                print("Invalid letter. Please, enter a single alphabetical")
-            elif guess in self.list_of_guesses:
-                print("You already tried that letter!")
-            else:
-                self.check_guess(guess)
-                self.list_of_guesses.append(guess)
+    def is_game_over(self):
+        if self.num_lives <= 0:
+            return True
+        elif '_' not in self.word_guessed:
+            return True
+        else:
+            return False
 
-# Test the hangman class
-hangman_game = hangman(["apple", 'banana', 'cherry'])
-hangman_game.ask_for_input()
+    def display_game_state(self):
+        print(f"Word: {' '.join(self.word_guessed)}")
+        print(f"Letters Guessed: {' '.join(self.list_of_guesses)}")
+        print(f"Lives Remaining: {self.num_lives}")
+
+    def play_game(self):
+        while not self.is_game_over():
+            self.display_game_state()
+            guess = input("Guess a letter: ")
+            self.check_guess(guess)
+            
+        if '_' not in self.word_guessed:
+            print(f"Congratulations! You've guessed the word: {self.word}")
+        else:
+            print(f"Out of lives. The word was: {self.word}")
+
+# Test the Hangman class
+word_list = ['apple', 'mango', 'avocado', 'lemon', 'durian']
+hangman_game = Hangman(word_list)
+hangman_game.play_game()
