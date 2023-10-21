b0ba9f9398cb11172bf1d951e3ec1a027e50c18a
abimcph <abimcph@yahoo.com>
2023-10-21 21:04:50 +0100

    Creating environment

---
+++ milestone_2.py
@@ -0,0 +1,21 @@
+# Create a list of favorite fruits and assign to a variable named word_list
+word_list =  ['mangosteen', 'mango', 'avocado', 'lemon', 'durian']
+
+# Print list
+print(word_list)
+
+# Import the random module
+import random
+
+# Generate a random word from the word_list
+word = random.choice(word_list)
+print(word)
+
+# Ask the user to enter a single letter
+guess = input("Please enter a single letter: ")
+
+# Check that the input is a single character
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
